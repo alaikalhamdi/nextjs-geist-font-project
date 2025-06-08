1b1c55922f1efa9be74a058ffd5031c1aa10b261
Alaikal Hamdi <yuan.arib@gmail.com>
2025-06-08 10:26:13 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749353173948

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__428a9a8a._.css",
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/src_components_Header_tsx_69d15ee6._.js"
+],
+    source: "dynamic"
+});
